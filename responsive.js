bc2f789f88ee1c7a78547a0fcdeed6c6e0f15f1d
unknown <BOSSANTONY>
2024-11-27 10:19:39 +0100

    Ajout des fichiers de mon site

---
+++ responsive.js
@@ -0,0 +1,26 @@
+// menu responsive code
+var menu = document.querySelector('.menu');
+var menu_toggle= document.querySelector('.menu_toggle');
+menu_toggle.onclick = function(){
+    menu_toggle.classList.toggle('active');
+    menu_toggle.classList.toggle('responsive');
+} 
+
+//site animation
+const header = document.querySelector('header');
+const title_span  =document.querySelector('.left h1 span'
+);
+const p = document.querySelector('.left p ');
+const a = document.querySelector('.left a ');
+
+const img= document.querySelector('.left ?image_s ');
+window.addEventListener('load', ()=>{
+    const TL = gsap.timeline({paused: true});
+    TL
+    .staggerFrom(header , 2 , {y:-100 , opacity:0, ease: "power2.out"}, 0.1)
+    .staggerFrom(img , 1, {x:1000 , opacity:0, ease: "power2.out"}, 0.3)
+    .staggerFrom(title_span , 1 , { opacity:0, ease: "power2.out"}, 0.3)
+    .staggerFrom(a , 1 , { opacity:0, ease: "power2.out"}, 0.3)
+    .staggerFrom(p , 1 , { opacity:0, ease: "power2.out"}, 0.3)
+    TL.play()
+})
